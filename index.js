f84170e5a30c19afe5f7b2281820bd4150329dff
plusemon <bdemon00@gmail.com>
2022-10-30 23:56:39 +0600

    push location api

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ function initMap() {
     var marker
     var infowindow
     var myLatlng = new google.maps.LatLng(23.8103, 90.4125)
-    // var geocoder = new google.maps.Geocoder()
-    // var infowindow = new google.maps.InfoWindow()
     var myMap = document.getElementById("map")
-
 
     map = new google.maps.Map(myMap, {
         zoom: 13,
@@ -21,7 +18,7 @@ function initMap() {
     })
 
     infowindow = new google.maps.InfoWindow({
-        content: 'Hi, what do you want here?',
+        content: 'Sabuj Vila',
         ariaLabel: "hahaha",
     })
 
@@ -54,51 +51,5 @@ function initMap() {
         $('#longitude').val(marker.getPosition().lng())
     }
 
-    // google.maps.event.addListener(marker, 'dragend', function () {
-    //     geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
-    //         if (status == google.maps.GeocoderStatus.OK) {
-    //             if (results[0]) {
-    //                 var address_components = results[0].address_components
-    //                 var components = {}
-    //                 jQuery.each(address_components, function (k, v1) { jQuery.each(v1.types, function (k2, v2) { components[v2] = v1.long_name }) })
-    //                 var city
-    //                 var postal_code
-    //                 var state
-    //                 var country
-    //                 if (components.locality) {
-    //                     city = components.locality
-    //                 }
-    //                 if (!city) {
-    //                     city = components.administrative_area_level_1
-    //                 }
-    //                 if (components.postal_code) {
-    //                     postal_code = components.postal_code
-    //                 }
-    //                 if (components.administrative_area_level_1) {
-    //                     state = components.administrative_area_level_1
-    //                 }
-    //                 if (components.country) {
-    //                     country = components.country
-    //                 }
 
-    //                 $.ajax({
-    //                     url: 'url',
-    //                     data: { state: state, country: country },
-    //                     type: 'POST',
-    //                     success: function (data) {
-    //                         $('#input-country').val(data['country'])
-    //                         $('#input-zone').val(data['zone'])
-    //                     }
-    //                 })
-    //                 $('#input-city').val(city)
-    //                 $('#input-postcode').val(postal_code)
-    //                 $('#latitude').val(marker.getPosition().lat())
-    //                 $('#longitude').val(marker.getPosition().lng())
-    //                 infowindow.setContent(results[0].formatted_address)
-    //                 infowindow.open(map, marker)
-    //             }
-    //         }
-    //     })
-    // })
 }
-
